e10750193252ab1d440581ba2af1b888cac83c47
TheRealJRad <42502544+TheRealJRad@users.noreply.github.com>
2023-11-10 14:13:51 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -21,7 +21,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
-      "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      // "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Rad",
       "managerid": "709158109351841792",
       // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
