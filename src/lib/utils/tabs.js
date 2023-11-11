e323d7f5956908e9bad0e0030a75b98d524554b2
TheRealJRad <42502544+TheRealJRad@users.noreply.github.com>
2023-11-11 12:50:48 -0600

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -63,7 +63,7 @@ export const tabs = [
             },
             {
                 icon: 'history_edu',
-                label: 'Constitution',
+                label: 'Charter',
                 dest: '/constitution',
             },
             {
