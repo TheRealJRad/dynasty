3577169904416254b11f1340b4d7b5920ba2d0e3
TheRealJRad <42502544+TheRealJRad@users.noreply.github.com>
2023-11-11 11:48:09 -0600

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,21 +1,3 @@
-<script>
-    import { dues } from '$lib/utils/helper';
-    let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
-    let two, twoOne, twoTwo, twoThree;
-    let three, threeOne;
-    let four, fourOne, fourTwo;
-    let five, fiveOne;
-    let six, sixOne, sixTwo, sixThree;
-    let seven, sevenOne, sevenTwo, sevenThree;
 
-    const goToSection = (section) => {
-        const top = section.getBoundingClientRect().top + window.pageYOffset;
-        window.scrollTo({left: 0, top, behavior: 'smooth'});
-    }
-</script>
-
-
-
-<div class="constitution">
    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTiShGe9GmMsjYyQ_JzYXfp_htNdf09XBji1Ue4KVg0Vk-UeuL-7A70ieCV99WJG56cfZVWNRrd_ZJn/pub?embedded=true"></iframe>
-</div>
+
