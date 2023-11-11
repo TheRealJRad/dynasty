ddc6b329ac930162f7d4138fcdc914ec1837f076
TheRealJRad <42502544+TheRealJRad@users.noreply.github.com>
2023-11-11 11:55:23 -0600

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -96,6 +96,6 @@
 </style>
 
 <div class="constitution">
-   <iframe width='100%' height='1500' src="https://docs.google.com/document/d/e/2PACX-1vTiShGe9GmMsjYyQ_JzYXfp_htNdf09XBji1Ue4KVg0Vk-UeuL-7A70ieCV99WJG56cfZVWNRrd_ZJn/pub?embedded=true"></iframe>
+   <iframe width='200%' height='1500' src="https://docs.google.com/document/d/e/2PACX-1vTiShGe9GmMsjYyQ_JzYXfp_htNdf09XBji1Ue4KVg0Vk-UeuL-7A70ieCV99WJG56cfZVWNRrd_ZJn/pub?embedded=true"></iframe>
 
 </div>
