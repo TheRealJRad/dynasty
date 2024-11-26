006b08829755fec37d1d6df63e446ba647d9a578
TheRealJRad <42502544+TheRealJRad@users.noreply.github.com>
2024-11-26 07:27:08 -0600

    Update leagueInfo.js
    update to 2024 ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "990372167906893824"; // your league ID
+export const leagueID = "1048651172120317952"; // your league ID
 export const leagueName = "This Dynasty League is Missing a"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
